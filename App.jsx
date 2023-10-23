59d9a25560e32edc913179825c3a76cbff79e785
achramdani <achramdani18@gmail.com>
2023-10-23 20:21:04 +0700

    kedua

--- App.jsx
+++ App.jsx
@@ -1,34 +1,42 @@
 import { StyleSheet, Text, View, Image , TextInput } from 'react-native'
 import React ,{ useState, useEffect} from 'react'
 const App = () => {
-  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [text, setText] = useState('');
   
   return (
     <View style={{flex: 1, backgroundColor: '#dbe4f3'}}>
       <View style={{justifyContent: 'center', alignItems: 'center'}}>
         <Image
          source={require('./src/images/facebook.png')} 
-         style={{width: 100, height: 100}}
+         style={{width: 100, height: 100, marginBottom:20,marginTop:50}}
         />
         <Text style={{fontSize: 18, fontWeight: 'bold'}}>FACEBOOK</Text>
         <Text style={{marginTop: 10, fontWeight: 'bold'}}>Login</Text>
       </View>
       <TextInput
-        value={email}
+        style={{
+          backgroundColor: '#FFFFFF',
+          marginHorizontal: 20, 
+          borderRadius: 15,
+          marginBottom:10,
+      }}
+        placeholder="Email"
+        onChangeText={(newText) => setText(newText)}
+        value={text}
+      />
+      <TextInput
         style={{
           backgroundColor: '#FFFFFF',
           marginHorizontal: 20, 
           borderRadius: 15,
       }}
-      placeholder="Masukkan Email"/>
-      <TextInput value={password} />
+        placeholder="Password"
+        onChangeText={(newPassword) => setPassword(newPassword)}
+        value={password}
+      />
     </View>
   );
 };
 
 export default App
-
-const styles = StyleSheet.create({
-  
-})
